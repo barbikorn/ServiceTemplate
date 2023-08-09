887da018f623433dceea6d0773bdcabc02b618d7
korn <kornsupakrit@gmail.com>
2023-08-09 11:30:43 +0700

    v 0 no login host

--- app/models/users/user.py
+++ app/models/users/user.py
@@ -1,11 +1,40 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Union
+
 
 class User(BaseModel):
-    id : Optional[str]
     name: str
+    username : str
     email: str
     password: str
+    role: str
     age: int
-    address: Optional[str] = None
-    
+    address: Optional[str] 
+
+class UserCreate(BaseModel):
+    name: str
+    username : str
+    email: str
+    password: str
+    role: Optional[str] = "users"
+    age: Optional[int] = 0
+    address: Optional[str] 
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str]
+    username : Optional[str]
+    email: Optional[str]
+    password: Optional[str]
+    role: Optional[str]
+    age: Optional[int]
+    address: Optional[str] 
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Union[str, None] = None
